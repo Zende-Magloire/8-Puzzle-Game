13d87cbe4d92f752d82eb19958c5bc76a972b284
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2023-02-14 12:32:16 -0500

    you won

--- scripts/script_table.js
+++ scripts/script_table.js
@@ -36,12 +36,12 @@ function elems_exchange(i, j) {
     console.log(isEmpty.toString);
 
     //testing stuff
-    console.log(`i = ${i}, j = ${j}`)
+    //console.log(`i = ${i}, j = ${j}`)
     //valE = " "
     // console.log(val1 === valE)
     //position of what was pressed
-    var pos1 = `${i} , ${j}`;
-    console.log(`pos1 = ${pos1}`)
+    //var pos1 = `${i} , ${j}`;
+    //console.log(`pos1 = ${pos1}`)
 
     //original code
     //  let k = j + 1;
@@ -56,28 +56,28 @@ function elems_exchange(i, j) {
     //if above/below (i+/-1) empty, switch horizontal
     //above
     let a = i - 1;
-    let above = `${a}, ${j}`;
-    console.log(`above= ${above}`);
+    // let above = `${a}, ${j}`;
+    // console.log(`above= ${above}`);
 
     //below
     let b = i + 1;
-    let below = `${b}, ${j}`;
-    console.log(`below= ${below}`);
+    // let below = `${b}, ${j}`;
+    //console.log(`below= ${below}`);
 
     //right
     let r = j + 1;
-    let right = `${i}, ${r}`;
-    console.log(`right= ${right}`);
+    //let right = `${i}, ${r}`;
+    //console.log(`right= ${right}`);
 
     //left
     let l = j - 1;
-    let left = `${i}, ${l}`;
-    console.log(`left= ${left}`);
+    // let left = `${i}, ${l}`;
+    //console.log(`left= ${left}`);
 
     //if above/below (i+/-1) empty, switch vertical
     if (b < table.rows.length) {
-        console.log("yes");
-        console.log(`below: ${table.rows[b].cells[j].innerHTML}`)
+        //  console.log("yes");
+        //  console.log(`below: ${table.rows[b].cells[j].innerHTML}`)
         if (table.rows[b].cells[j].innerHTML === " ")
         //or if below === isEmpty
         {
@@ -87,8 +87,8 @@ function elems_exchange(i, j) {
         }
     }
     if (a >= 0) {
-        console.log("yes");
-        console.log(`above: ${table.rows[a].cells[j].innerHTML}`)
+        // console.log("yes");
+        //console.log(`above: ${table.rows[a].cells[j].innerHTML}`)
         if (table.rows[a].cells[j].innerHTML === " ")
         //or if (above === isEmpty)
         {
@@ -100,8 +100,8 @@ function elems_exchange(i, j) {
 
     // if right/left (j+/-1) empty, switch horizontal
     if (r <= 2) {
-        console.log("yes");
-        console.log(`right: ${table.rows[i].cells[r].innerHTML}`)
+        // console.log("yes");
+        //console.log(`right: ${table.rows[i].cells[r].innerHTML}`)
         if (table.rows[i].cells[r].innerHTML === " ")
         //or if (right === isEmpty)
         {
@@ -111,8 +111,8 @@ function elems_exchange(i, j) {
         }
     }
     if (l >= 0) {
-        console.log("yes");
-        console.log(`left: ${table.rows[i].cells[l].innerHTML}`)
+        //  console.log("yes");
+        // console.log(`left: ${table.rows[i].cells[l].innerHTML}`)
         if (table.rows[i].cells[l].innerHTML === " ")
         //or if (left === isEmpty)
         {
@@ -124,33 +124,34 @@ function elems_exchange(i, j) {
     else {
         return
     }
-
     //is it solved?
-    console.log(solved(table))
+    if (solved(table))
+        alert("You won!");
 }
+
 
 function isEmpty() {
     let valE = " ";
     for (let i = 0; i < table.rows.length; i++) {
-        for (let j = 0; i < table.rows[i].cells; j++)
-            if (table.rows[i].cells[j].innerHTML.toString === valE) {
+        for (let j = 0; i < 3; j++)
+            if (table.rows[i].cells[j].innerHTML === valE) {
                 // var empty = `${i} , ${j}`;
-                return i, j;
+                console.log(`${i} , ${j}`);
             }
     }
 }
 
 function solved(table) {
     if (
-        table.rows[0].cells[0].innerHTML.toString === "1" &&
-        table.rows[0].cells[1].innerHTML.toString === "2" &&
-        table.rows[0].cells[2].innerHTML.toString === "3" &&
-        table.rows[1].cells[2].innerHTML.toString === "4" &&
-        table.rows[2].cells[2].innerHTML.toString === "5" &&
-        table.rows[2].cells[1].innerHTML.toString === "6" &&
-        table.rows[2].cells[0].innerHTML.toString === "7" &&
-        table.rows[1].cells[0].innerHTML.toString === "8" &&
-        table.rows[1].cells[1].innerHTML.toString === " "
+        table.rows[0].cells[0].innerHTML === "1" &&
+        table.rows[0].cells[1].innerHTML === "2" &&
+        table.rows[0].cells[2].innerHTML === "3" &&
+        table.rows[1].cells[2].innerHTML === "4" &&
+        table.rows[2].cells[2].innerHTML === "5" &&
+        table.rows[2].cells[1].innerHTML === "6" &&
+        table.rows[2].cells[0].innerHTML === "7" &&
+        table.rows[1].cells[0].innerHTML === "8" &&
+        table.rows[1].cells[1].innerHTML === " "
     )
         return true;
     else
