3c9ebf1bff67bcdda6f5eb8cff8bfe5f56ef15ba
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2023-02-15 23:21:52 -0500

    final - clean up

--- scripts/script_table.js
+++ scripts/script_table.js
@@ -8,23 +8,6 @@ window.onload = function () {
     });
 }
 
-/*
-function elems_exchange(i, j) {
-    const table = document.querySelector('table');
-    const val1 = table.rows[i].cells[j].innerHTML;
-
-    let k = j + 1;
-    let numRows = table.rows.length; // not used, but this gets num rows
-    if (k > table.rows[i].cells.length - 1) {
-        k = 0;
-    }
-    const val2 = table.rows[i].cells[k].innerHTML;
-
-    table.rows[i].cells[j].innerHTML = val2.toString();
-    table.rows[i].cells[k].innerHTML = val1.toString();
-}
-*/
-
 function elems_exchange(i, j) {
     const table = document.querySelector('table');
 
@@ -34,52 +17,22 @@ function elems_exchange(i, j) {
     //find empty cell
     let empty = isEmpty(table);
 
-    //testing stuff
-    //console.log(`i = ${i}, j = ${j}`)
-    //valE = " "
-    // console.log(val1 === valE)
-    //position of what was pressed
-    //var pos1 = `${i} , ${j}`;
-    //console.log(`pos1 = ${pos1}`)
-    console.log(`val1 = ${val1}`)
-    console.log(empty)
-
-    //original code
-    //  let k = j + 1;
-    //  let numRows = table.rows.length; // not used, but this gets num rows
-    //  if (k > table.rows[i].cells.length - 1) {
-    //    k = 0;
-    // }
-
-    //const val2 = table.rows[i].cells[k].innerHTML;
-    //console.log(`val2 = ${val2}`)
-
     //what to switch with
     //above
     let a = i - 1;
     let above = `${a}, ${j}`;
-    // console.log(`above= ${above}`);
-
     //below
     let b = i + 1;
     let below = `${b}, ${j}`;
-    //console.log(`below= ${below}`);
-
     //right
     let r = j + 1;
     let right = `${i}, ${r}`;
-    //console.log(`right= ${right}`);
-
     //left
     let l = j - 1;
     let left = `${i}, ${l}`;
-    //console.log(`left= ${left}`);
 
     //if above/below (i+/-1) empty, switch vertical
     if (b < table.rows.length) {
-        //console.log("yes");
-        //  console.log(`below: ${table.rows[b].cells[j].innerHTML}`)
-        // if (table.rows[b].cells[j].innerHTML === " ")
         if (below === empty) {
             const val2 = table.rows[b].cells[j].innerHTML;
             table.rows[i].cells[j].innerHTML = val2.toString();
@@ -87,21 +40,14 @@ function elems_exchange(i, j) {
         }
     }
     if (a >= 0) {
-        // console.log("yes");
-        //console.log(`above: ${table.rows[a].cells[j].innerHTML}`)
-        // if (table.rows[a].cells[j].innerHTML === " ")
         if (above === empty) {
             const val2 = table.rows[a].cells[j].innerHTML;
             table.rows[i].cells[j].innerHTML = val2.toString();
             table.rows[a].cells[j].innerHTML = val1.toString();
         }
     }
-
     // if right/left (j+/-1) empty, switch horizontal
     if (r <= 2) {
-        // console.log("yes");
-        //console.log(`right: ${table.rows[i].cells[r].innerHTML}`)
-        // if (table.rows[i].cells[r].innerHTML === " ")
         if (right === empty) {
             const val2 = table.rows[i].cells[r].innerHTML;
             table.rows[i].cells[j].innerHTML = val2.toString();
@@ -109,9 +55,6 @@ function elems_exchange(i, j) {
         }
     }
     if (l >= 0) {
-        //  console.log("yes");
-        // console.log(`left: ${table.rows[i].cells[l].innerHTML}`)
-        //if (table.rows[i].cells[l].innerHTML === " ")
         if (left === empty) {
             const val2 = table.rows[i].cells[l].innerHTML;
             table.rows[i].cells[j].innerHTML = val2.toString();
@@ -135,13 +78,10 @@ function elems_exchange(i, j) {
 
 //find empty cell
 function isEmpty(table) {
-    //const table = document.querySelectorAll('table');
     let valE = " ";
     for (let i = 0; i < table.rows.length; i++) {
         for (let j = 0; j < table.rows.length; j++) {
             if (table.rows[i].cells[j].innerHTML === valE) {
-                // let empty = `${i} , ${j}`;
-                //console.log(`${i} , ${j}`);
                 return `${i}, ${j}`;
             }
         }
@@ -172,24 +112,22 @@ const btn = document.querySelector("button");
 btn.addEventListener("click", scrabble);
 
 function scrabble() {
-
     const table = document.querySelector('table');
     var text = document.getElementById("h2");
     text.innerHTML = "You are in the game!"
     text.style.color = "#808080";
     text.style.border = "2px dotted pink";
-
     btn.innerHTML = "New Scramble"
 
     let rand = 0;
     let count = 0;
+
     while (count <= 35) {
         //find empty cell
         let empty = isEmpty(table);
         let i = parseInt(empty[0]);
         let j = parseInt(empty[3]);
         const val1 = table.rows[i].cells[j].innerHTML;
-        //console.log(`val1 = ${val1}`);
 
         //find surrounding cells
         //above
